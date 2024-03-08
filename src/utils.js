c78f9e410b5586db1ee81178d80f104c82565691
Thilakmani <thilakmani@kissflow.com>
2024-03-08 09:58:11 +0530

    fix: :sparkles: added util to check node and push it on drag

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,40 @@
-export const findObjectById = (id, obj) => {
-  if (obj.id === id) {
-    return obj;
+export function moveObjectByIdToTeam(objectId, toTeamName, orgData) {
+  const moveObjectRecursive = (node) => {
+    if (node.children && node.children.length > 0) {
+      for (let i = 0; i < node.children.length; i++) {
+        const child = node.children[i];
+        if (child.id === objectId) {
+          const [objectToMove] = node.children.splice(i, 1);
+
+          const targetTeamNode = findTeamNode(orgData, toTeamName);
+          if (targetTeamNode) {
+            targetTeamNode.children.push(objectToMove);
+          }
+          return orgData;
+        }
+
+        const result = moveObjectRecursive(child);
+        if (result) {
+          return result;
+        }
+      }
+    }
+    return null;
+  };
+  return moveObjectRecursive(orgData);
+}
+
+function findTeamNode(node, teamName) {
+  if (node.id === teamName) {
+    return node;
   }
-  if (obj.children) {
-    for (const child of obj.children) {
-      const result = findObjectById(id, child);
+  if (node.children) {
+    for (const child of node.children) {
+      const result = findTeamNode(child, teamName);
       if (result) {
         return result;
       }
     }
   }
   return null;
-};
+}
